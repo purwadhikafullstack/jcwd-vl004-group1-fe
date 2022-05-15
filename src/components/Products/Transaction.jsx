f565fdb0577c58a2340dbcb22d13a19c8479dea5
ADIP1598 <dhiyailmamputra@gmail.com>
2022-05-15 10:26:31 +0700

    Quick Fix

--- src/components/Products/Transaction.jsx
+++ src/components/Products/Transaction.jsx
@@ -52,7 +52,7 @@ const Transaction = () => {
                     <td>{val.number}</td>
                     <td>{val.invoice_header.warehouse.name}</td>
                     <td>{val.invoice_header.user_address.province}</td>
-                    <td>Waiting for Stock</td>
+                    <td>{val.status}</td>
                     <td>
                       <div className="my-2 space-x-1">
                           <button class="bg-transparent hover:bg-teal-500 text-teal-700 font-semibold hover:text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded">
@@ -67,85 +67,6 @@ const Transaction = () => {
             )
             }
         )
-        // return data.map((val, i) => {
-        //   return (
-        //     <tr key={i}>
-        //       <td>{i+1}</td>
-        //       <td>{val.full_name}</td>
-        //       <td>{val.username}</td>
-        //       <td>{val.email}</td>
-        //       <td>{val.phone}</td>
-        //       <td>{val.is_verified ? (
-        //         <div><i className="fas fa-check-square"></i></div>
-        //       ):(
-        //         <div className="fa fa-times"></div>
-        //       )}</td>
-        //       <td>{val.is_active ? (
-        //         <button class="btn btn-success btn-sm btn-outline">Active</button>
-        //       ):(
-        //         <button class="btn btn-error btn-sm btn-outline">Inactive</button>
-        //       )}</td>
-        //       <td>
-        //         {val.is_active ? (
-        //           <>
-        //            <label for="my-modal-1" class="btn btn-error btn-sm btn-outline"><i class="fa fa fa-power-off" aria-hidden="true"></i></label>
-        //           </>
-        //         ):(
-        //           <>
-        //           <label for="my-modal-2" class="btn btn-success btn-sm btn-outline"><i class="fa fa fa-check" aria-hidden="true"></i></label>
-                  
-        //           </>              
-        //         )}
-        //       </td>
-        //       <input type="checkbox" id="my-modal-1" class="modal-toggle"/>
-        //         <div class="modal modal-bottom sm:modal-middle">
-        //           <div class="relative p-4 w-full max-w-md h-full md:h-auto">
-        //             {/* <!-- Modal content --> */}
-        //             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-        //                 {/* <!-- Modal header --> */}
-        //                 <div class="flex justify-end p-2">
-        //                     <label for="my-modal-1" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-teal" data-modal-toggle="popup-modal">
-        //                         <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
-        //                     </label>
-        //                 </div>
-        //                 {/* <!-- Modal body --> */}
-        //                 <div class="p-6 pt-0 text-center">
-        //                     <svg class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-        //                     <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-black-400 text-break">Are you sure you want to deactivate {val.full_name}'s account?</h3>
-        //                     <label for="my-modal-1" data-modal-toggle="popup-modal" onClick={()=>changestatus("deactivated",val.id)} type="button" class="text-white bg-teal-600 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-        //                         Yes, I'm sure
-        //                     </label>
-        //                     <label for="my-modal-1" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-teal dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</label>
-        //                 </div>
-        //             </div>
-        //           </div>
-        //         </div>
-        //         <input type="checkbox" id="my-modal-2" class="modal-toggle"/>
-        //           <div class="modal modal-bottom sm:modal-middle">
-        //             <div class="relative p-4 w-full max-w-md h-full md:h-auto">
-        //               {/* <!-- Modal content --> */}
-        //               <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-        //                   {/* <!-- Modal header --> */}
-        //                   <div class="flex justify-end p-2">
-        //                       <label for="my-modal-2" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-teal" data-modal-toggle="popup-modal">
-        //                           <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
-        //                       </label>
-        //                   </div>
-        //                   {/* <!-- Modal body --> */}
-        //                   <div class="p-6 pt-0 text-center">
-        //                       <svg class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-        //                       <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-black-400 text-break">Are you sure you want to activate {val.full_name}'s account?</h3>
-        //                       <label for="my-modal-2" data-modal-toggle="popup-modal" onClick={()=>changestatus("activated",val.id)} type="button" class="text-white bg-teal-600 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-        //                           Yes, I'm sure
-        //                       </label>
-        //                       <label for="my-modal-2" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-teal dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</label>
-        //                   </div>
-        //               </div>
-        //             </div>
-        //           </div>
-        //     </tr>
-        //   );
-        // });
       };
 
     return (
